c5ca3de7cc6a792b5a33c8cc37e0079940c73410
Gylmar Gonzalez <coopervisual@hotmail.es>
2024-08-12 18:33:19 -0600

    base proyect

---
+++ src/main/java/com/vadmin/VadminApplication.java
@@ -0,0 +1,13 @@
+package com.vadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VadminApplication.class, args);
+	}
+
+}
